74020925c019900b781b7f3891f9dbb503afe020
DLSLIsaac <128102874+DLSLIsaac@users.noreply.github.com>
2025-05-22 10:42:24 -0600

    Twig usage and basics

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     // Attribute
     #[Route('/')]
     public function homePage(): Response
     {
-        return new Response('<strong>StarShop</strong>: your monopoly-busting option for Starship parts!');
+      $starshipCount = 457;
+
+      $myShip = [
+        'name' => 'USS LeafyCruiser (NCC-0001)',
+        'class' => 'Garden',
+        'captain' => 'Jean-Luc Pickles',
+        'status' => 'under construction'
+      ];
+
+      return $this->render('main/homepage.html.twig', [
+        'numberOfStarships' => $starshipCount,
+        'myShip' => $myShip
+      ]);
     }
 }
